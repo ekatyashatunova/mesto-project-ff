8696b599f9b6549cc8b1a87185df5d4b2abb64aa
shatunidze <ekatyashatunova@yandex.ru>
2024-05-17 22:58:48 +0300

    modal.js 17.05

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,34 +1,34 @@
 //Функция открытия попапа
- export function openPopup(popupOpen) {
-    popupOpen.classList.add('popup_is-opened');
-    document.addEventListener('keydown', closePopupEsc);
-    document.addEventListener('click', closeByOverlay);
+export function openPopup(popupOpen) {
+    popupOpen.classList.add("popup_is-opened");
+    document.addEventListener("keydown", closePopupEsc);
+    document.addEventListener("click", closeByOverlay);
 }
 
 //Функция закрытия попапа
 export function closePopup(popupClose) {
-     popupClose.classList.remove('popup_is-opened');
-    const closeButtonPopup = document.querySelectorAll('.popup__close');
+    popupClose.classList.remove("popup_is-opened");
+    const closeButtonPopup = document.querySelectorAll(".popup__close");
     closeButtonPopup.forEach((button) => {
-        button.addEventListener('click', () => {
-        popupClose.classList.remove('popup_is-opened');
+        button.addEventListener("click", () => {
+            popupClose.classList.remove("popup_is-opened");
         });
     });
-    }
-    document.removeEventListener('keydown', closePopupEsc);
-    document.removeEventListener('click', closeByOverlay);
+}
+document.removeEventListener("keydown", closePopupEsc);
+document.removeEventListener("click", closeByOverlay);
 
 //Функция закрытия попапа по оверлею
 export function closeByOverlay(evt) {
-    if (evt.target.classList.contains('popup_is-opened')) {
+    if (evt.target.classList.contains("popup_is-opened")) {
         closePopup(evt.target);
     }
 }
 
 //Функция закрытия попапа по кнопке ESC
- export function closePopupEsc(evt) {
-    if (evt.key ==='Escape') {
-        const popupOpened = document.querySelector('.popup_is-opened');
+export function closePopupEsc(evt) {
+    if (evt.key === "Escape") {
+        const popupOpened = document.querySelector(".popup_is-opened");
         closePopup(popupOpened);
     }
 }
