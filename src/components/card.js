856c512762d3a23279b519b3327724ea40d69b3d
shatunidze <ekatyashatunova@yandex.ru>
2024-05-20 22:33:41 +0300

    правки после второго ревью card.js 20.05

--- src/components/card.js
+++ src/components/card.js
@@ -1,27 +1,27 @@
 //Темплейт карточки
-const cardTemplate = document.querySelector("#card-template").content;
+const cardTemplate = document.querySelector('#card-template').content;
 
 //Функция создания карточек
 
 export function createCardElement(card, deleteCardCall, likeCallBack, openImage) {
-    const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
-    const imageElement = cardElement.querySelector(".card__image");
-    const deleteButton = cardElement.querySelector(".card__delete-button");
-    const titleElement = cardElement.querySelector(".card__title");
-    const likeButton = cardElement.querySelector(".card__like-button");
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const imageElement = cardElement.querySelector('.card__image');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    const titleElement = cardElement.querySelector('.card__title');
+    const likeButton = cardElement.querySelector('.card__like-button');
     imageElement.src = card.link;
     imageElement.alt = card.name;
     titleElement.textContent = card.name;
 
-    deleteButton.addEventListener("click", () => {
+    deleteButton.addEventListener('click', () => {
         deleteCardCall(cardElement);
     });
 
-    likeButton.addEventListener("click", () => {
+    likeButton.addEventListener('click', () => {
         likeCallBack(likeButton);
     });
 
-    imageElement.addEventListener("click", () => {
+    imageElement.addEventListener('click', () => {
         openImage(card.link, card.name);
     })
 
@@ -34,10 +34,10 @@ export function deleteCardElement(cardElement) {
 }
 
 export function likeCard(likeButton) {
-    if (likeButton.classList.add("card__like-button_is-active")) {
-        likeButton.classList.remove("card__like-button_is-active");
+    if (likeButton.classList.add('card__like-button_is-active')) {
+        likeButton.classList.remove('card__like-button_is-active');
     } else {
-        likeButton.classList.add("card__like-button_is-active");
+        likeButton.classList.add('card__like-button_is-active');
     }
 }
 
