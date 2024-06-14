1d4bfd7d6bf0cd5016fc018163b11ffb2fcba28e
shatunidze <ekatyashatunova@yandex.ru>
2024-06-14 19:40:23 +0300

    изменения в api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,72 +1,59 @@
 const configAPI = {
-    baseUrl: "https://nomoreparties.co/v1/сohort-13/",
+    baseUrl: "https://nomoreparties.co/v1/wff-cohort-16",
   headers: {
     authorization: "d6861576-603e-40be-9c84-d05dd645bb31",
     "Content-Type": "application/json",
   },
 }
 
+const handleResponse = (response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(`Что-то пошло не так: ${response.status}`); 
+}
+
 //Получение информации о пользователе с сервера
 export const getEditProfile = () => {
   return fetch(`.${configAPI.baseUrl}/users/me`, {
-    method: 'GET',
+    method: "GET",
     headers: configAPI.headers,
   }) 
-  .then(res => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Что-то пошло не так: ${res.status}`); 
-  })
+  .then(handleResponse);
 }
 
 //Получение инфы о карточках с сервера
-export const getInitialCards = () => {
+export const getInitialCard = () => {
   return fetch(`.${configAPI.baseUrl}/cards`, {
-    method: 'GET',
+    method: "GET",
     headers: configAPI.headers,
   })
-  .then(res => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Что-то пошло не так: ${res.status}`); 
-  })
+  .then(handleResponse)
 }
 
 //Редактирование профиля и отправка данных на сервер
-export const editProfileToServer = () => {
-  return fetch(`.${configAPI.baseUrl}/users/me`, {
-    method: 'PATCH',
+export const editProfileToServer = (nameInput, jobInput) => {
+  return fetch(`${configAPI.baseUrl}/users/me`, {
+    method: "PATCH",
     headers: configAPI.headers,
     body: JSON.stringify({
     name: nameInput.value,
-    about: jobInput.value
-  })
+    about: jobInput.value,
+  }),
 })
-.then(res => {
-  if (res.ok) {
-    return res.json();
-  }
-  return Promise.reject(`Что-то пошло не так: ${res.status}`); 
-})
+.then(handleResponse)
 }
 
 //Добавление новой карточки и отправка на сервер
-export const newCardToServer = () => {
-  return fetch(`.${configAPI.baseUrl}/cards`, {
+export const newCardToServer = (newCard) => {
+  return fetch(`${configAPI.baseUrl}/cards`, {
     method: 'POST',
     headers: configAPI.headers,
     body: JSON.stringify({
-      name: cardNameInput.value,
-      link: cardLinkInput.value
-    })
+      name: newCard.name,
+      link: newCard.link
+    }),
   })
-  .then(res => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Что-то пошло не так: ${res.status}`); 
-  })
+  .then(handleResponse)
 }
 
