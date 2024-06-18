cbe4a57ef1992f56e267d41daf3cacef74227efd
shatunidze <ekatyashatunova@yandex.ru>
2024-06-19 00:05:06 +0300

    изменения в api.js 18/06

--- src/components/api.js
+++ src/components/api.js
@@ -58,20 +58,41 @@ export const newCardToServer = (newCard) => {
 }
 
 //Удаление карточки
+export const deleteCardId = (cardId) => {
+  return fetch(`${configAPI.baseUrl}/cards/${cardId}`, {
+    method: 'DELETE',
+    headers: configAPI.headers,
+})
+.then(handleResponse)
+}
 
 //Постановка лайка
+export const likeCardId = (cardId) => {
+  return fetch(`${configAPI.baseUrl}/cards/likes/${cardId}`, {
+    method: 'PUT',
+    headers: configAPI.headers,
+})
+.then(handleResponse)
+}
 
 //Снятие лайка
+export const unlikeCardId = (cardId) => {
+  return fetch(`${configAPI.baseUrl}/cards/likes/${cardId}`, {
+    method: 'DELETE',
+    headers: configAPI.headers,
+})
+.then(handleResponse)
+} 
+
 
 //Обновление аватара пользователя
-export const updateAvatar = (avatar) => {
+/*export const updateAvatar = (profileAvatarInput) => {
   return fetch(`${configAPI.baseUrl}/users/me/avatar`, {
     method: 'PATCH',
     headers: configAPI.headers,
     body: JSON.stringify({
-      link: avatar.link
+      avatar: profileAvatarInput.value
     }),
-})
-.then(handleResponse)
-}
+  })
+}*/
 
